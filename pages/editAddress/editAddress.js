2cf8140506e641f06a8dc8862993b912e03a36e8
朱利云 <fd@qq.com>
2021-09-29 19:12:01 +0800

    update

--- pages/editAddress/editAddress.js
+++ pages/editAddress/editAddress.js
@@ -17,111 +17,32 @@ Page(
 
       areaAddress: '',
       areaList: [],
-      provinceList: [{
-        id: 110000,
-        name: '北京',
-        areas: [
-          {
-            code: 101,
-            name: '朝阳区'
-          },
-          {
-            code: 101,
-            name: '丰台区'
-          }
-        ]
-      }, {
-        code: 2,
-        name: '河北省',
-        citys: [
-          {
-            code: 20,
-            name: '石家庄市',
-            areas: [{
-              code: '201',
-              name: '长安区'
-            }]
-          }
-        ]
-      }], // 省份列表
-      cityList: [{
-        code: 1,
-        name: '北京',
-        areas: [
-          {
-            code: 101,
-            name: '朝阳区'
-          },
-          {
-            code: 101,
-            name: '丰台区'
-          }
-        ]
-      }, {
-        code: 2,
-        name: '河北省',
-        citys: [
-          {
-            code: 20,
-            name: '石家庄市',
-            areas: [{
-              code: '201',
-              name: '长安区'
-            }]
-          }
-        ]
-      }], // 市县列表
-      districtList: [{
-        code: 1,
-        name: '北京',
-        areas: [
-          {
-            code: 101,
-            name: '朝阳区'
-          },
-          {
-            code: 101,
-            name: '丰台区'
-          }
-        ]
-      }, {
-        code: 2,
-        name: '河北省',
-        citys: [
-          {
-            code: 20,
-            name: '石家庄市',
-            areas: [{
-              code: '201',
-              name: '长安区'
-            }]
-          }
-        ]
-      }], // 区县列表
+      provinceList: [], // 省份列表
+      cityList: [], // 市县列表
+      districtList: [], // 区县列表
       areaIndex: [0, 0, 0], // 索引
     },
-
     // 初始化三级地区
-    initArea(areaList) {
+    initArea() {
       var self = this;
       // 初始化数据
       let initIndex = self.data.areaIndex
       let provinceId, cityId, areaId, provinceName, cityName, areaName
-      provinceId = areaList[initIndex[0]].id
-      provinceName = areaList[initIndex[0]].name
-      cityId = areaList[initIndex[0]].children[initIndex[1]].id
-      cityName = areaList[initIndex[0]].children[initIndex[1]].name
-      areaId = areaList[initIndex[0]].children[initIndex[1]].children[initIndex[2]].id
-      areaName = areaList[initIndex[0]].children[initIndex[1]].children[initIndex[2]].name
+      provinceId = areaList.data[initIndex[0]].id
+      provinceName = areaList.data[initIndex[0]].name
+      cityId = areaList.data[initIndex[0]].children[initIndex[1]].id
+      cityName = areaList.data[initIndex[0]].children[initIndex[1]].name
+      areaId = areaList.data[initIndex[0]].children[initIndex[1]].children[initIndex[2]].id
+      areaName = areaList.data[initIndex[0]].children[initIndex[1]].children[initIndex[2]].name
       // 初始化列表数据
       self.setData({
         'form.province': provinceId,
         'form.city': cityId,
         'form.area': areaId,
         areaAddress: provinceName + cityName + areaName,
-        provinceList: areaList,
-        cityList: areaList[0].children,
-        districtList: areaList[0].children[0].children
+        provinceList: areaList.data,
+        cityList: areaList.data[0].children,
+        districtList: areaList.data[0].children[0].children
       })
     },
     // 选择地址——填充选择框——填充上传数据
@@ -135,6 +56,7 @@ Page(
       cityName = self.data.provinceList[initIndex[0]].children[initIndex[1]].name
       areaId = self.data.provinceList[initIndex[0]].children[initIndex[1]].children[initIndex[2]].id
       areaName = self.data.provinceList[initIndex[0]].children[initIndex[1]].children[initIndex[2]].name
+
       self.setData({
         'form.province': provinceId,
         'form.city': cityId,
@@ -149,13 +71,24 @@ Page(
       let newIndexArr = e.detail.value
       // 监听第一列
       if (newIndexArr[0] !== self.data.areaIndex[0]) {
-        // 滑动第一列——初始化第二列和第三列的数据
-         self.httpGetCertifyInfo(newIndexArr[0])
-        let initArr = [newIndexArr[0], 0, 0]
-        self.setData({
-          areaIndex: initArr,
-          cityList: self.data.provinceList[initArr[0]].children,
-          districtList: self.data.provinceList[initArr[0]].children[0].children
+        // 滑动第一列——初始化第二列和第三列的数据]
+        base.http.httpPost({
+          url: "userAddress/getProvinces",
+          data: { pid: self.data.provinceList[newIndexArr[0]].id },
+          success(res) {
+            let areaList = res.data;
+            areaList.map((item, index) => {
+              item.name = item.cname;
+              delete item.cname
+            })
+
+            let initArr = [newIndexArr[0], 0, 0]
+            self.setData({
+              areaIndex: initArr,
+              cityList: areaList,
+              districtList: [],
+            })
+          }
         })
         return
       }
@@ -163,9 +96,20 @@ Page(
       if (newIndexArr[1] !== self.data.areaIndex[1]) {
         // 滑动第一列——初始化第二列和第三列的数据
         let initArr = [newIndexArr[0], newIndexArr[1], 0]
-        self.setData({
-          areaIndex: initArr,
-          districtList: self.data.provinceList[initArr[0]].children[initArr[1]].children
+        base.http.httpPost({
+          url: "userAddress/getProvinces",
+          data: { pid: self.data.cityList[newIndexArr[1]].id },
+          success(res) {
+            let areaList = res.data;
+            areaList.map((item, index) => {
+              item.name = item.cname;
+              delete item.cname
+            })
+            self.setData({
+              areaIndex: initArr,
+              districtList: areaList,
+            })
+          }
         })
         return
       }
@@ -174,32 +118,33 @@ Page(
         areaIndex: newIndexArr
       })
     },
+
+
     showAreaPicker() {
       this.setData({
         showPopup: true,
       });
     },
-
     onLoad(query) {
       // 页面加载
       this.httpGetCertifyInfo(0);
-      this.httpGetBankList();
-      if (query.id) {
-        this.setData({
-          id: query.id
-        });
-        this.httpGetList(query.id);
-      }
-      if (query.orderData) {
-        this.setData({
-          orderData: JSON.parse(query.orderData)
-        });
-      }
-      if (query.orderId) {
-        this.setData({
-          orderId: query.orderId
-        });
-      }
+      // this.httpGetBankList();
+      // if (query.id) {
+      //   this.setData({
+      //     id: query.id
+      //   });
+      //   this.httpGetList(query.id);
+      // }
+      // if (query.orderData) {
+      //   this.setData({
+      //     orderData: JSON.parse(query.orderData)
+      //   });
+      // }
+      // if (query.orderId) {
+      //   this.setData({
+      //     orderId: query.orderId
+      //   });
+      // }
     },
     onReady() {
       // 页面加载完成
@@ -295,18 +240,36 @@ Page(
       base.http.httpPost({
         url: "userAddress/getProvinces",
         data: { pid },
-        success(res) {
+        success(result) {
           // _that.setData({
           //   realname: res.data.realname,
           //   cardNo: res.data.cardNo
           // });
-          let areaList = res.data;
+          let areaList = result.data;
           areaList.map((item, index) => {
             item.name = item.cname;
             delete item.cname
           })
-          console.log(areaList)
-          _that.initArea(areaList)
+          _that.setData({
+            provinceList: areaList
+          })
+          base.http.httpPost({
+            url: "userAddress/getProvinces",
+            data: { pid: result.data[0].id },
+            success(res) {
+              let areaList1 = res.data;
+              areaList1.map((item, index) => {
+                item.name = item.cname;
+                delete item.cname
+              })
+              _that.setData({
+                cityList: areaList1
+              })
+            }
+          })
+          // _that.setData({
+          //   provinceList: areaList
+          // })
         }
       });
     },
